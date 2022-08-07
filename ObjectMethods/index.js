4442cd95409efcc1f9cbfd2c9c58cbca92586140
João Araújo <joaogabriel-al@hotmail.com>
2022-08-07 18:09:23 -0300

    Object Keys

---
+++ ObjectMethods/index.js
@@ -0,0 +1,11 @@
+// Object Methods
+// keys - Retorna um array com as propriedades do objeto
+
+let obj = {
+  name: "Gabriel",
+  age: 28,
+  job: "Programmer"
+}
+
+let objKeys = Object.keys(obj)
+objKeys
